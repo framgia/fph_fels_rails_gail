40773ec4db6578b23ef3adf06a8ab9b4798c75d7
gaily <gailchan16@gmail.com>
2019-02-08 15:18:36 +0800

    layout pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  root 'static_pages#home' 
+	root 'static_pages#home'
+	get '/signup', to: 'users#new'
+ 	get '/login', to: 'sessions#new'
+ 	get '/category', to: 'categories#home' 
 end
