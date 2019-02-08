f9c946fe3c804b02f81c7ae6268289136b3e2783
gaily <gailchan16@gmail.com>
2019-02-08 19:35:38 +0800

    signup page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,12 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+
+$( document ).ready(function() {
+    $('#password, #confirm_password').on('keyup', function () {
+        if ($('#password').val() == $('#confirm_password').val()) {
+            $('#message').html('Matching').css('color', 'green');
+        } else 
+            $('#message').html('Not Matching').css('color', 'red');
+        });
+});
