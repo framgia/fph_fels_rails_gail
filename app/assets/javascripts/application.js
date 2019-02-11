d50a83aa18209b06288bae6cc1eb0c630443f999
gaily <gailchan16@gmail.com>
2019-02-11 10:40:44 +0800

    validation changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,9 +19,9 @@
 
 $( document ).ready(function() {
     $('#password, #confirm_password').on('keyup', function () {
-        if ($('#password').val() == $('#confirm_password').val()) {
+        if($('#password').val() == $('#confirm_password').val()) {
             $('#message').html('Matching').css('color', 'green');
-        } else 
+        }else 
             $('#message').html('Not Matching').css('color', 'red');
-        });
+    });
 });
