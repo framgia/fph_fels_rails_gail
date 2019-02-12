51ff3f701f65acb0f1741b6511e8b0e0568e7eae
gaily <gailchan16@gmail.com>
2019-02-12 17:29:07 +0800

    deleted unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,10 @@ Rails.application.routes.draw do
   
   #navbar pages
  	get '/login', to: 'sessions#new'
-  get '/admin/category', to: 'admin/categories#index'
   get '/signup', to: 'users#new'
-  get '/admin/category', to: 'admin/categories#new'  
 
   post '/signup', to: 'users#create'
   post '/login',   to: 'sessions#create'
-  post '/admin/category', to: 'admin/categories#create'
 
   delete '/logout',  to: 'sessions#destroy'
   
