c7cb21ce82e85ace6ed7c32b4308111a29246639
gaily <gailchan16@gmail.com>
2019-02-13 12:48:39 +0800

    display,edit and delete category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,16 +1,41 @@
 class Admin::CategoriesController < ApplicationController
+
+    def index 
+      @categories = Category.paginate(page: params[:page], per_page:10)
+    end
+
     def new
-        @category = Category.new 
+      @category = Category.new 
     end
     
     def create
-        @category = Category.new(categ_params)
-        if @category.save
-            redirect_to admin_categories_url
-        else
-          render 'new'
-        end
+      @category = Category.new(categ_params)
+      if @category.save
+        redirect_to admin_categories_url
+      else
+        render 'new'
       end
+    end
+
+    def edit 
+      @category = Category.find(params[:id])
+    end
+
+    def update 
+      @category = Category.find(params[:id])
+      if @category.update_attributes(categ_params)
+        flash[:success] = "Category #{@category.id.to_s} has been updated!" 
+        redirect_to admin_categories_url
+      else
+        render 'edit'
+      end 
+    end
+
+    def destroy 
+      Category.find(params[:id]).destroy
+      flash[:success] = "Category was Deleted!"
+      redirect_to admin_categories_url
+    end
 
     private
     def categ_params
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190212032222) do
+ActiveRecord::Schema.define(version: 20190213043656) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20190212032222) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin"
   end
 
 end
