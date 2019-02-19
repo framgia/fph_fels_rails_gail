5232ca511a53d1afca03b71c43b5fd87317d0ad1
gaily <gailchan16@gmail.com>
2019-02-19 18:36:09 +0800

    category user

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,6 @@
 class CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.paginate(page: params[:page], per_page:10).where("EXISTS(SELECT 1 from words where categories.id = words.category_id)")
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
     has_many :words, dependent: :destroy
     validates :title, presence: true, length: { maximum: 50 }
-    validates :description, presence: true, length: { minimum: 50, maximum: 150 }
+    validates :description, presence: true, length: { minimum: 50, maximum: 90 }
 end
