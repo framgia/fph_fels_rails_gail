e8d528e8327d35b41ea57aeca0fb1d17ab4a0bbe
gaily <gailchan16@gmail.com>
2019-03-05 18:02:20 +0800

    fixed indentation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,16 +1,16 @@
 class Category < ApplicationRecord
-    has_many :words, dependent: :destroy
-    validates :title, presence: true, length: { maximum: 50 }
-    validates :description, presence: true, length: { minimum: 50, maximum: 90 }
-    has_many :lessons
+  has_many :words, dependent: :destroy
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :description, presence: true, length: { minimum: 50, maximum: 90 }
+  has_many :lessons
 
-    #retrieves the all the categories that has words 
-    def self.words_exist
-       Category.where("EXISTS(SELECT 1 from words where categories.id = words.category_id)")
-    end
+  #retrieves the all the categories that has words 
+  def self.words_exist
+    Category.where("EXISTS(SELECT 1 from words where categories.id = words.category_id)")
+  end
     
-    def lesson_score(user)
-        lesson = lessons.find_by(user_id: user.id)
-        "#{lesson.score} of #{lesson.answers.count} "
-    end
+  def lesson_score(user)
+    lesson = lessons.find_by(user_id: user.id)
+    "#{lesson.score} of #{lesson.answers.count} "
+  end
 end
