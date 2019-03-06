87702cb4c493b371a86daea67df6ebc066ad8205
gaily <gailchan16@gmail.com>
2019-03-06 14:43:46 +0800

    activity in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190304052808) do
+ActiveRecord::Schema.define(version: 20190305075837) do
+
+  create_table "activities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "action_type"
+    t.bigint "action_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["action_type", "action_id"], name: "index_activities_on_action_type_and_action_id"
+    t.index ["user_id"], name: "index_activities_on_user_id"
+  end
 
   create_table "answers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.bigint "word_id"
@@ -78,6 +88,7 @@ ActiveRecord::Schema.define(version: 20190304052808) do
     t.index ["category_id"], name: "index_words_on_category_id"
   end
 
+  add_foreign_key "activities", "users"
   add_foreign_key "answers", "choices"
   add_foreign_key "answers", "lessons"
   add_foreign_key "answers", "words"
